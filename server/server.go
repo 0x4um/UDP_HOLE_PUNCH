a9d0d32a62a6d41d6a4eaecee4f754b207100f78
gwptross <gwptross@gmail.com>
2023-09-05 08:05:10 -0600

    this

--- server/server.go
+++ server/server.go
@@ -4,9 +4,67 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
 )
+
+func checkTable(db *sql.DB, tableName string) (bool, error) {
+	query := fmt.Sprintf("SELECT name FROM sqlite_master WHERE type='table' AND name='%s'", tableName)
+	rows, err := db.Query(query)
+	if err != nil {
+		return false, err 
+	
+	}
+	defer rows.Close()
+
+	return rows.Next(), nil
+}
+
 func main(){
 	
+	db, err := sql.Open("sqlite3", "db.db")
+	if err != nil {
+		fmt.Println("error opening")
+		return
+	}
+	defer db.Close()
+
+	tableName := "users"
+
+	exists, err := checkTable(db, tableName)
+	if err != nil {
+		fmt.Println("error checking", err)
+		return
+	}
+
+	if exists {
+		fmt.Println("the table exists")
+
+	} else {
+		fmt.Println("table does not exists.. creating one")
+		createTableSql := `
+			CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			ip TEXT NOT NULL,
+			other TEXT NOT NULL
+		)
+		`
+		
+		_, err := db.Exec(createTableSql)
+		if err != nil {
+			fmt.Println("unable to create table", err)
+			return
+		}
+	}
+
+	rows, err := db.Query("SELECT id FROM users")
+	if err != nil {
+		fmt.Println("error select")
+		return
+	}
+	defer rows.Close()
+
+
 	ln, err := net.Listen("tcp", ":12000")
 	if err != nil {
 		fmt.Println("error starting")
@@ -43,8 +101,8 @@ func handleConn(conn net.Conn){
 		dataToCheck := data[:5]
 		if strings.TrimSpace(dataToCheck) == "hello" {
 			fmt.Println("hello world", data)
-			localAddr := conn.LocalAddr().String()
-			_, err := conn.Write([]byte(localAddr))
+			RemoteAddr := conn.RemoteAddr().String()
+			_, err := conn.Write([]byte(RemoteAddr))
 			if err != nil {
 				fmt.Println("unable to write")
 				return
