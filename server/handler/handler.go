416c416672454dddfaa86d965c09e0b7becd9d22
gavin <gwptross@gmail.com>
2023-09-09 17:37:49 -0600

    this

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -2,9 +2,31 @@ package handler
 
 import (
 	"fmt"
+	"strings"
+	"net"
 )
 
-func Exec(data string) bool {
-	fmt.Println("hello from exec", data)
-	return true
+func function1() {
+	fmt.Println("function1 hello")
+}
+
+func function2() {
+	fmt.Println("function2 hello")
+}
+
+func Exec(data string, n int, conn net.Conn) {
+	fmt.Println(data)
+	fmt.Println(n)
+	functions := map[string]func(){
+		"function1": function1,
+		"function2": function2,
+	}
+	// buffer := make([]byte, 1024)
+	other := strings.TrimSpace(data)
+	fmt.Println(other, " other other")
+	if fn, exists := functions[other]; exists {
+		fn()
+	} else {
+		fmt.Println("func not found")
+	}
 }
